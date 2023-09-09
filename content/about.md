b6cacd2ebeec2252840f31cede15c1e08a608ba3
kihea <kihea@icloud.com>
2023-09-09 15:55:41 -0500

    first commit

---
+++ content/about.md
@@ -0,0 +1,53 @@
+---
+title: "About"
+layout: about
+draft: false
+
+## about us
+about_us:
+  subtitle: WHO WE ARE
+  title: Hello, We’re Bellaire Games Studio
+  content: At the heart of our club, we believe in the power of collaboration, creativity, and learning. Whether you're an aspiring game designer, a skilled programmer, a talented artist, or just someone who loves games, you'll find a welcoming space to share your ideas and skills.
+  image: /images/about/01.jpg
+
+## our works
+works:
+  subtitle: OUR WORKS
+  title: What We Do
+  list:
+    - title: Teach Game Design
+      content: Learn by doing! We teach game design through a series of hands-on and beginner-friendly projects.
+    - title: Host Game Jams
+      content: Want to showcase your skills? Join us for a game jam! We host game jams at least once every semester, and we're always looking for new participants. All you need is a passion for games and a willingness to learn.
+    - title: Host Workshops
+      content: No matter what you're interested in, we have a workshop for you. We've hosted workshops on game design, programming, art, music, and more. We've also hosted workshops on game development tools like Unity and Unreal Engine.
+    - title: Make Games
+      content: Fundamentally, we believe that game development is a skill that can be learned and practiced. We believe that games are a powerful medium for creative expression, and that the best way to learn about games is to make them.
+
+## our mission
+mission:
+  subtitle: OUR MISSION
+  title: Inspiring Play, Empowering Creators
+  content: Our mission is to inspire a love for games and game development while educating students in high-value skills. By offering a collaborative space, expert guidance, and a diverse range of projects, we aim to foster a community where innovation thrives and members can bring their ideas to life.
+  image: /images/about/02.jpg
+
+
+## members
+our_member:
+  subtitle: OUR MEMBERS
+  title: The People Behind
+  content: We were freelance designers and developers, constantly finding </br> ourselves deep in vague feedback. This made every client and team
+  list:
+    - name: Kihea Adams-Wilson
+      field: President
+      image: /images/about/team/01.jpg
+    - name: Heldana Yacob
+      field: Vice President
+      image: /images/about/team/02.jpg
+    - name: Mihea Adams
+      field: Manager
+      image: /images/about/team/03.jpg
+
+
+---
+
---
+++ layouts/About.js
@@ -0,0 +1,191 @@
+import { markdownify } from "@lib/utils/textConverter";
+import { Autoplay } from "swiper";
+import { Swiper, SwiperSlide } from "swiper/react";
+import Banner from "./components/Banner";
+import Circle from "./components/Circle";
+import Cta from "./components/Cta";
+import ImageFallback from "./components/ImageFallback";
+import VideoPopup from "./components/VideoPopup";
+
+const About = ({ data }) => {
+  const { frontmatter } = data;
+  const {
+    title,
+    about_us,
+    works,
+    mission,
+    our_member,
+  } = frontmatter;
+
+  return (
+    <>
+      <section className="section pt-0">
+        <Banner title={title} />
+        {/* About */}
+        <div className="section container">
+          <div className="row items-center justify-center">
+            <div className="animate md:col-6 lg:col-5 md:order-2">
+              <div className="about-image relative p-[60px]">
+                <ImageFallback
+                  className="animate relative w-full rounded-2xl"
+                  src={about_us.image}
+                  width={425}
+                  height={487}
+                  alt=""
+                />
+                <Circle
+                  className="top-4 left-4 z-[-1]"
+                  width={85}
+                  height={85}
+                />
+                <Circle
+                  width={37}
+                  height={37}
+                  fill={false}
+                  className="top-20 right-10 z-[-1]"
+                />
+                <Circle
+                  className="top-1/2 right-12 -z-[1]"
+                  width={24}
+                  height={24}
+                />
+                <Circle
+                  className="bottom-6 right-6 z-[-1]"
+                  width={85}
+                  height={85}
+                />
+                <Circle
+                  className="top-1/2 left-12 z-[-1]"
+                  width={20}
+                  height={20}
+                />
+                <Circle
+                  className="bottom-12 left-8 z-[1]"
+                  width={47}
+                  height={47}
+                  fill={false}
+                />
+              </div>
+            </div>
+            <div className="animate md:col-6 lg:col-4 md:order-1">
+              <p>{about_us.subtitle}</p>
+              {markdownify(about_us.title, "h2", "section-title bar-left mt-4")}
+              {markdownify(about_us.content, "p", "mt-10")}
+            </div>
+          </div>
+        </div>
+
+        {/* Works */}
+        <div className="section container">
+          <div className="animate text-center">
+            <p>{works.subtitle}</p>
+            {markdownify(works.title, "h2", "section-title mt-4")}
+            {markdownify(works.content, "p", "mt-10")}
+          </div>
+          <div className="row mt-10 justify-center">
+            {works.list.map((work, index) => (
+              <div key={"work-" + index} className="mt-10 md:col-6 lg:col-5">
+                <div className="animate text-center md:px-6 xl:px-12">
+                  {markdownify(work.title, "h3", "h4")}
+                  {markdownify(work.content, "p", "mt-2")}
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+
+        {/* Mission */}
+        <div className="section container">
+          <div className="row items-center justify-center">
+            <div className="animate md:col-6 lg:col-5">
+              <div className="about-image relative p-[60px]">
+                <ImageFallback
+                  className="animate relative w-full rounded-2xl"
+                  src={mission.image}
+                  width={425}
+                  height={487}
+                  alt=""
+                />
+                <Circle
+                  className="top-4 left-4 z-[-1]"
+                  width={85}
+                  height={85}
+                />
+                <Circle
+                  width={37}
+                  height={37}
+                  fill={false}
+                  className="top-20 right-10 z-[-1]"
+                />
+                <Circle
+                  className="top-1/2 right-12 -z-[1]"
+                  width={24}
+                  height={24}
+                />
+                <Circle
+                  className="bottom-6 right-6 z-[-1]"
+                  width={85}
+                  height={85}
+                />
+                <Circle
+                  className="top-1/2 left-12 z-[-1]"
+                  width={20}
+                  height={20}
+                />
+                <Circle
+                  className="bottom-12 left-8 z-[1]"
+                  width={47}
+                  height={47}
+                  fill={false}
+                />
+              </div>
+            </div>
+            <div className="animate md:col-6 lg:col-4">
+              <p>{mission.subtitle}</p>
+              {markdownify(mission.title, "h2", "section-title bar-left mt-4")}
+              {markdownify(mission.content, "p", "mt-10")}
+            </div>
+          </div>
+        </div>
+
+        
+
+        
+
+        {/* Members */}
+        <div className="section container">
+          <div className="animate text-center">
+            <p>{our_member.subtitle}</p>
+            {markdownify(our_member.title, "h2", "section-title mt-4")}
+            {markdownify(our_member.content, "p", "mt-16")}
+          </div>
+          <div className="row justify-center">
+            <div className="lg:col-10">
+              <div className="row">
+                {our_member.list.map((member, index) => (
+                  <div
+                    key={("member-", index)}
+                    className="animate mt-10 text-center md:col-6 lg:col-4"
+                  >
+                    <ImageFallback
+                      className="mx-auto rounded-full shadow-[10px_10px_0] shadow-primary/10"
+                      src={member.image}
+                      width={245}
+                      height={245}
+                      alt={member.name}
+                    />
+                    <h4 className="mt-8">{member.name}</h4>
+                    <p className="mt-3">{member.field}</p>
+                  </div>
+                ))}
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <Cta />
+    </>
+  );
+};
+
+export default About;
